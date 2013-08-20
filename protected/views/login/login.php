792a053e23344b097e7796cb50279ec265197d0f
zhutaofen <575956381@qq.com>
2013-08-20 23:39:20 +0800

    add login

--- protected/views/login/login.php
+++ protected/views/login/login.php
@@ -7,3 +7,16 @@
  * To change this template use File | Settings | File Templates.
  */
 ?>
+<div class="container">
+
+    <form class="form-signin">
+        <h4 class="form-signin-heading">登陆兜兜网</h4>
+        <input type="text" class="input-block-level" placeholder="用户名">
+        <input type="password" class="input-block-level" placeholder="密码">
+        <label class="checkbox">
+            <input type="checkbox" value="remember-me"> 一个月内自动登录
+        </label>
+        <button class="btn btn-media btn-info" type="submit">登陆</button>
+    </form>
+
+</div> <!-- /container -->
