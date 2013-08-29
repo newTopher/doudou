f2a04ec2e3047b98840f691dc2830387b2bd8d3d
topher eric <575956381@qq.com>
2013-08-29 17:29:32 +0800

    update home index

--- protected/views/index/index.php
+++ protected/views/index/index.php
@@ -33,6 +33,7 @@ $this->pageTitle=Yii::app()->name;
 </div>
 
 <div class="container">
+    <!--start top-->
     <div class="row-fluid topContainer">
         <div class="span2">
             <span class="headerBox">
@@ -89,4 +90,146 @@ $this->pageTitle=Yii::app()->name;
             </div>
         </div>
     </div>
+    <!--end top-->
+
+    <!--start center-->
+    <div class="row">
+        <div class="span2">
+            <div class="userLeftBar">
+                <ul class="nav nav-pills nav-stacked">
+                    <li class="active"><a href="">我的照片</a></li>
+                    <li><a href="">玩得好的</a></li>
+                    <li><a href="">大家在玩</a></li>
+                    <li><a href="">牵线搭桥</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="span7">
+            <div class="userPubCenter">
+                <div class="stautsButton">
+                    <ul class="inline">
+                        <li><a href="javascript:void(0)">好友在说</a></li>
+                        <li><a href="javascript:void(0)">和我相关的</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="weiboListBox">
+                <div class="conWrap clear">
+                    <div class="userHeaderPic">
+                        <img class="img-rounded" src="http://img.zealer.com/50/50/cc9cc00288b50c4f7bb5c6513433be63dc0.jpg" alt="头像">
+                    </div>
+                    <div class="conText">
+                        <div class="tAreaBox">
+                            <div class="userLitleInfo">
+                                <span class="mb_name"><a href="">DeliChan</a></span>
+                                <span class="sign">  Keep each other warm</span>
+                            </div>
+                            <div class="ps">
+                                <div class="content">
+                                    <p>
+                                        想问下哈，9月10号不是发布iphone5s嘛，我想入手iphone5，我知道5s一出，iphone5价格肯定会跳水，5s出了之后，多久入手iphone5合适呢，你觉得5的价格大概会降到什么价位呢？ 在哪儿买比较好？可以解答下吗？现在的iphone5还会掉漆吗，黑色版的，
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="otherUserToolBar">
+                                <span class="fromTime">49分钟前</span>
+                                <a href="javascript:;">赞(12)</a>
+                                <a href="javascript:;">评论(15)</a>
+                                <a href="javascript:;">转发(25)</a>
+                            </div>
+                        </div>
+                        <div class="commentBox">
+                            <ul class="unstyled">
+                                <li>
+                                    <div class="comUserHeaderPic">
+                                        <img src="http://img.zealer.com/50/50/cc9cc00288b50c4f7bb5c6513433be63dc0.jpg" alt="头像">
+                                    </div>
+                                    <div class="comUserNick">
+                                        <a href="">陈晓冰</a>
+                                    </div>
+                                    <div class="userComContents">
+                                        <span>
+                                            我喜欢你这样子的回答我喜欢你这样子的回答我喜欢你这样子的回答我喜欢你这样子的回答
+                                        </span>
+                                    </div>
+                                    <div class="userComTime">
+                                        <span class="fromTime">3分钟前</span><a href="javascript:;">回复</a>
+                                    </div>
+
+                                </li>
+                            </ul>
+                            <div class="addWrap">
+                                <textarea class="userComTextarea" placeholder="在此输入评论内容"></textarea>
+                                <div class="userComToolsBar">
+                                    <ul class="inline">
+                                        <li class="userComButtonLi"><button class="btn userComButton" type="button">回复</button></li>
+                                        <li><a href="javascript:void(0)">表情</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="conWrap clear">
+                    <div class="userHeaderPic">
+                        <img class="img-rounded" src="http://img.zealer.com/50/50/cc9cc00288b50c4f7bb5c6513433be63dc0.jpg" alt="头像">
+                    </div>
+                    <div class="conText">
+                        <div class="tAreaBox">
+                            <div class="userLitleInfo">
+                                <span class="mb_name"><a href="">DeliChan</a></span>
+                                <span class="sign">  Keep each other warm</span>
+                            </div>
+                            <div class="ps">
+                                <div id="q_ps_3180" class="content">
+                                    <p>
+                                        今天去哪里玩呢，你觉得怎么样啊
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="otherUserToolBar">
+                                <span class="fromTime">49分钟前</span>
+                                <a href="javascript:;">赞(12)</a>
+                                <a href="javascript:;">评论(15)</a>
+                                <a href="javascript:;">转发(25)</a>
+                            </div>
+                        </div>
+                        <div class="commentBox">
+                            <ul class="unstyled">
+                                <li>
+                                    <div class="comUserHeaderPic">
+                                        <img src="http://img.zealer.com/50/50/cc9cc00288b50c4f7bb5c6513433be63dc0.jpg" alt="头像">
+                                    </div>
+                                    <div class="comUserNick">
+                                        <a href="">陈晓冰</a>
+                                    </div>
+                                    <div class="userComContents">
+                                        <span>
+                                            估计你是神了！！！
+                                        </span>
+                                    </div>
+                                    <div class="userComTime">
+                                        <span class="fromTime">3分钟前</span><a href="javascript:;">回复</a>
+                                    </div>
+
+                                </li>
+                            </ul>
+                            <div class="addWrap">
+                                <textarea class="userComTextarea" placeholder="在此输入评论内容"></textarea>
+                                <div class="userComToolsBar">
+                                    <ul class="inline">
+                                        <li class="userComButtonLi"><button class="btn userComButton" type="button">回复</button></li>
+                                        <li><a href="javascript:void(0)">表情</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="span3">span3</div>
+    </div>
+    <!--end center-->
 </div>
