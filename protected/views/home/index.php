07e61c5dc809ffe0a441bee388e79139edb4835c
zhutaofen <575956381@qq.com>
2013-09-29 01:48:43 +0800

    add home index

--- protected/views/home/index.php
+++ protected/views/home/index.php
@@ -30,11 +30,169 @@
 
     <div class="indexmMain">
         <div class="indexMainTop">
-
+            <div class="homebannerimg">
+                <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/home/sum1.jpg">
+            </div>
         </div>
 
         <div class="indexMainCenter">
+            <div class="centerTop">
+                <div class="home-block-head">
+                    <h1>兜兜的小伙伴们</h1>
+                </div>
+                <div class="userimgblock">
+                    <ul class="home-goods-list home-goods-listlt clearfix">
+                        <li class="big">
+                            <a href="http://www.xiaomi.com/goods/1676" target="_blank" onclick="_gaq.push(['_trackEvent','首页', 'B1_0','http://www.xiaomi.com/goods/1676']);_msq.push(['trackEvent',event,'H', 'H-A-B1_0','http://www.xiaomi.com/goods/1676'])">
+                                <img alt="移动电源" src="http://p.www.xiaomi.com/images/xmFocus/yddy099.jpg" style="opacity: 1;">
+                            </a>
+                        </li>
 
+                        <li class="mediali">
+                            <a href="http://huosai.xiaomi.com/" target="_blank" onclick="_gaq.push(['_trackEvent','首页', 'B1_1','http://huosai.xiaomi.com/']);_msq.push(['trackEvent',event,'H', 'H-A-B1_1','http://huosai.xiaomi.com/'])">
+                                <img alt="活塞耳机" src="http://p.www.xiaomi.com/images/xmFocus/huosaierji824.jpg" style="opacity: 1;">
+                            </a>
+                        </li>
+
+                        <li class="mediali">
+                            <a href="http://www.xiaomi.com/goods/1971" target="_blank" onclick="_gaq.push(['_trackEvent','首页', 'B1_2','http://www.xiaomi.com/goods/1971']);_msq.push(['trackEvent',event,'H', 'H-A-B1_2','http://www.xiaomi.com/goods/1971'])">
+                                <img alt="暗彩版电池盖（暗红）" src="http://p.www.xiaomi.com/images/xmFocus/ruizhiancai925.jpg" style="opacity: 1;">
+                            </a>
+                        </li class="mediali">
+
+                        <li class="mediali">
+                            <a href="http://www.xiaomi.com/goods/2502" target="_blank" onclick="_gaq.push(['_trackEvent','首页', 'B1_3','http://www.xiaomi.com/goods/2502']);_msq.push(['trackEvent',event,'H', 'H-A-B1_3','http://www.xiaomi.com/goods/2502'])">
+                                <img alt="MI2A变形金刚保护套（狂派红）" src="http://p.www.xiaomi.com/images/xmFocus/bianxingjingang925.jpg" style="opacity: 1;">
+                            </a>
+                        </li>
+
+
+                        <li class="mediali">
+                            <a href="http://www.xiaomi.com/goods/2681" target="_blank" onclick="_gaq.push(['_trackEvent','首页', 'B1_4','http://www.xiaomi.com/goods/2681']);_msq.push(['trackEvent',event,'H', 'H-A-B1_4','http://www.xiaomi.com/goods/2681'])">
+                                <img alt="乐泡移动电源6000mAh" src="http://p.www.xiaomi.com/images/xmFocus/lepao927.jpg" style="opacity: 1;">
+                            </a>
+                        </li>
+                    </ul>
+                    <ul class="home-goods-list home-goods-listrt">
+                        <li class="small smallfirst">
+                            <a href="http://www.xiaomi.com/goods/1473" target="_blank" onclick="_gaq.push(['_trackEvent','首页', 'B1_5','http://www.xiaomi.com/goods/1473']);_msq.push(['trackEvent',event,'H', 'H-A-B1_5','http://www.xiaomi.com/goods/1473'])">
+                                <img alt="屏幕擦" src="http://p.www.xiaomi.com/images/xmFocus/pmc00923.jpg" style="opacity: 1;">
+                            </a>
+                        </li>
+
+                        <li class="small smallfirst">
+                            <a href="http://www.xiaomi.com/goods/1384" target="_blank" onclick="_gaq.push(['_trackEvent','首页', 'B1_6','http://www.xiaomi.com/goods/1384']);_msq.push(['trackEvent',event,'H', 'H-A-B1_6','http://www.xiaomi.com/goods/1384'])">
+                                <img alt="多彩电源适配器" src="http://p.www.xiaomi.com/images/xmFocus/shipeiqi925.jpg" style="opacity: 1;">
+                            </a>
+                        </li>
+
+                        <li class="small smallsublast">
+                            <a href="http://www.xiaomi.com/goods/4098" target="_blank" onclick="_gaq.push(['_trackEvent','首页', 'B1_7','http://www.xiaomi.com/goods/4098']);_msq.push(['trackEvent',event,'H', 'H-A-B1_7','http://www.xiaomi.com/goods/4098'])">
+                                <img alt="MIUI扑克" src="http://p.www.xiaomi.com/images/xmFocus/MIUIpuke918.jpg" style="opacity: 1;">
+                            </a>
+                        </li>
+
+                        <li class="small smalllast last">
+                            <a href="http://www.xiaomi.com/goods/2327" target="_blank" onclick="_gaq.push(['_trackEvent','首页', 'B1_8','http://www.xiaomi.com/goods/2327']);_msq.push(['trackEvent',event,'H', 'H-A-B1_8','http://www.xiaomi.com/goods/2327'])">
+                                <img alt="小米商务真皮名片夹" src="http://p.www.xiaomi.com/images/xmFocus/mingpianjia925.jpg" style="opacity: 1;">
+                            </a>
+                        </li>
+
+                        <li class="small smalllast last">
+                            <a href="http://www.xiaomi.com/goods/2327" target="_blank" onclick="_gaq.push(['_trackEvent','首页', 'B1_8','http://www.xiaomi.com/goods/2327']);_msq.push(['trackEvent',event,'H', 'H-A-B1_8','http://www.xiaomi.com/goods/2327'])">
+                                <img alt="小米商务真皮名片夹" src="http://p.www.xiaomi.com/images/xmFocus/mingpianjia925.jpg" style="opacity: 1;">
+                            </a>
+                        </li>
+
+                        <li class="small">
+                            <a href="http://www.xiaomi.com/goods/1384" target="_blank" onclick="_gaq.push(['_trackEvent','首页', 'B1_6','http://www.xiaomi.com/goods/1384']);_msq.push(['trackEvent',event,'H', 'H-A-B1_6','http://www.xiaomi.com/goods/1384'])">
+                                <img alt="多彩电源适配器" src="http://p.www.xiaomi.com/images/xmFocus/shipeiqi925.jpg" style="opacity: 1;">
+                            </a>
+                        </li>
+
+                        <li class="small smallsublast">
+                            <a href="http://www.xiaomi.com/goods/4098" target="_blank" onclick="_gaq.push(['_trackEvent','首页', 'B1_7','http://www.xiaomi.com/goods/4098']);_msq.push(['trackEvent',event,'H', 'H-A-B1_7','http://www.xiaomi.com/goods/4098'])">
+                                <img alt="MIUI扑克" src="http://p.www.xiaomi.com/images/xmFocus/MIUIpuke918.jpg" style="opacity: 1;">
+                            </a>
+                        </li>
+
+                        <li class="small smalllast last">
+                            <a href="http://www.xiaomi.com/goods/2327" target="_blank" onclick="_gaq.push(['_trackEvent','首页', 'B1_8','http://www.xiaomi.com/goods/2327']);_msq.push(['trackEvent',event,'H', 'H-A-B1_8','http://www.xiaomi.com/goods/2327'])">
+                                <img alt="小米商务真皮名片夹" src="http://p.www.xiaomi.com/images/xmFocus/mingpianjia925.jpg" style="opacity: 1;">
+                            </a>
+                        </li>
+
+                    </ul>
+
+
+                </div>
+            </div>
         </div>
+
+        <div class="indexBottom">
+            <div class="bottomLeft">
+                <div class="home-block-head">
+                    <h1>在一起</h1>
+                </div>
+                <ul>
+                    <li class="post_item">
+                        <div class="pic"><a href="/i/2855866.html" target="_blank"><img src="http://file25.mafengwo.net/M00/35/36/wKgB4lI_A0aAVluSAAuILv-nI4w25.groupinfo.w195.jpeg"></a><b class=""></b></div>
+                        <dl class="cf">
+                            <dd>
+                                <h2><a href="/i/2855866.html" target="_blank">暮云无声明月未出 中秋节的晚玉米</a></h2>
+                                <div class="post_info">
+                                    <p> 美貌、青春、财富、甚至爱情本身，都不能让深得上帝恩宠的人免于焦虑和痛苦，远离哀愁，也无法让他们避免失去自己最爱的东西。因为，一...</p>
+                                </div>
+                            </dd>
+                        </dl>
+                    </li>
+
+                    <li class="post_item">
+                        <div class="pic"><a href="/i/2855866.html" target="_blank"><img src="http://file25.mafengwo.net/M00/35/36/wKgB4lI_A0aAVluSAAuILv-nI4w25.groupinfo.w195.jpeg"></a><b class=""></b></div>
+                        <dl class="cf">
+                            <dd>
+                                <h2><a href="/i/2855866.html" target="_blank">暮云无声明月未出 中秋节的晚玉米</a></h2>
+                                <div class="post_info">
+                                    <p> 美貌、青春、财富、甚至爱情本身，都不能让深得上帝恩宠的人免于焦虑和痛苦，远离哀愁，也无法让他们避免失去自己最爱的东西。因为，一...</p>
+                                </div>
+                            </dd>
+                        </dl>
+                    </li>
+                </ul>
+            </div>
+            <div class="bottomRight">
+                <div class="home-block-head">
+                    <h1>创意礼物</h1>
+                </div>
+                <div class="xm-goods-list-wrap">
+                    <ul class="xm-goods-list clearfix">
+                        <li>
+                            <div class="xm-goods-item">
+                                <div class="item-thumb">
+                                    <a href="http://www.xiaomi.com/goods/1688" target="_blank" onclick="_gaq.push(['_trackEvent','首页', 'E2_0','http://www.xiaomi.com/goods/1688']);_msq.push(['trackEvent',event,'H', 'H-A-E2_0','http://www.xiaomi.com/goods/1688'])"><img alt="抽取式保护套" src="http://p.www.xiaomi.com/images/a/1688_1_1358151106_3.jpg" style="opacity: 1;"></a>
+                                </div>
+                                <div class="item-price">59元</div>
+                                <div class="item-name"><a href="http://www.xiaomi.com/goods/1688" target="_blank" onclick="_gaq.push(['_trackEvent','首页', 'E2_1','http://www.xiaomi.com/goods/1688']);_msq.push(['trackEvent',event,'H', 'H-A-E2_1','http://www.xiaomi.com/goods/1688'])">抽取式保护套</a></div>
+                            </div>
+                        </li>
+                        <li class="second">
+                            <div class="xm-goods-item">
+                                <div class="item-thumb">
+                                    <a href="http://www.xiaomi.com/goods/2530" target="_blank" onclick="_gaq.push(['_trackEvent','首页', 'E2_2','http://www.xiaomi.com/goods/2530']);_msq.push(['trackEvent',event,'H', 'H-A-E2_2','http://www.xiaomi.com/goods/2530'])"><img alt="小米手机2S经典套装" src="http://p.www.xiaomi.com/images/5/2530_1_1370598996_3.jpg" style="opacity: 1;"></a>
+                                </div>
+                                <div class="item-price">89元</div>
+                                <div class="item-name"><a href="http://www.xiaomi.com/goods/2530" target="_blank" onclick="_gaq.push(['_trackEvent','首页', 'E2_3','http://www.xiaomi.com/goods/2530']);_msq.push(['trackEvent',event,'H', 'H-A-E2_3','http://www.xiaomi.com/goods/2530'])">小米手机2S经典套装</a></div>
+                            </div>
+                        </li>
+
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="indexFooter">
+
     </div>
 </body>
